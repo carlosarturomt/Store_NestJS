ea33fa9324fdd53d38b3eafac19da34b98cf48a3
charly <charly@charlys-MacBook-Air.local>
2024-01-12 18:07:33 -0600

    clean Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import { Hero } from 'app/components/home/Hero'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Description />
-      <Hero /> 
+      <Hero />
     </main>
   )
 }
